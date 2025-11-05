675ec3a1fab149245bbb0ae6fd8a6522548c865a
AngusTheFirst <alanangus1@gmail.com>
2025-11-05 02:11:52 +0000

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,10 @@
+function getValues(){
+    console.log(price1);
+    return 0;
+}
+
+document.querySelector("form").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevents the page from refreshing
+});
+
+
